a9a3f1ebeac4e2c6a0513f4f95208deda76d7058
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-09-22 22:47:39 +0200

    Performance optimization when dependencies are static

--- tests/test_pyproject_dependencies.py
+++ tests/test_pyproject_dependencies.py
@@ -23,6 +23,27 @@ def _make_project(tmp_path: Path, name: str, deps: List[str]) -> None:
     )
 
 
+def _make_legacy_project(tmp_path: Path, name: str, deps: List[str]) -> None:
+    assert len(deps) > 0
+    project_path = tmp_path / name
+    project_path.mkdir()
+    setup_py = project_path / "setup.py"
+    deps_str = '", "'.join(deps)
+    setup_py.write_text(
+        textwrap.dedent(
+            f"""
+            from setuptools import setup
+
+            setup(
+                name="{name}",
+                version="1.0",
+                install_requires=["{deps_str}"],
+            )
+            """
+        )
+    )
+
+
 def test_basic(tmp_path: Path) -> None:
     _make_project(tmp_path, name="p1", deps=["d1", "d2"])
     _make_project(tmp_path, name="p2", deps=["d1", "d3", "p1"])
@@ -33,6 +54,24 @@ def test_basic(tmp_path: Path) -> None:
             "pyproject_dependencies",
             str(tmp_path / "p1"),
             str(tmp_path / "p2" / "pyproject.toml"),
+        ],
+        check=True,
+        text=True,
+        capture_output=True,
+    )
+    assert result.stdout == "d1\nd2\nd3\n"
+
+
+def test_basic_legacy(tmp_path: Path) -> None:
+    _make_legacy_project(tmp_path, name="p1", deps=["d1", "d2"])
+    _make_legacy_project(tmp_path, name="p2", deps=["d1", "d3", "p1"])
+    result = subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pyproject_dependencies",
+            str(tmp_path / "p1"),
+            str(tmp_path / "p2" / "setup.py"),
         ],
         check=True,
         text=True,
