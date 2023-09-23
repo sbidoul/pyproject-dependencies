36f630819e0c2e4b437672dd6eb414b51f9d5082
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-09-23 09:01:26 +0200

    Simplify, don't use pyproject_metadata dependency

--- src/pyproject_dependencies/compat.py
+++ src/pyproject_dependencies/compat.py
@@ -1,14 +1,9 @@
 import sys
 
-if sys.version_info < (3, 8):
-    from typing_extensions import Protocol
-else:
-    from typing import Protocol
-
 if sys.version_info < (3, 11):
     import tomli as tomllib
 else:
     import tomllib
 
 
-__all__ = ["tomllib", "Protocol"]
+__all__ = ["tomllib"]
